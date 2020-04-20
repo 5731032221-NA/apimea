4b80d5c754dc3cf58877f4a9bb570154a98bb921
msc-dts <naphol159@gmail.com>
2020-04-20 10:57:17 +0700

    maxprop

--- app.js
+++ app.js
@@ -197,6 +197,29 @@ app.post('/postmeaprofile/:id', cors(issue2options), function (req, res) {
   });
 });
 
+app.post('/postmeapic/:id', cors(issue2options), function (req, res) {
+
+  const uri = "mongodb://localhost:27017/";
+  //, { useNewUrlParser: true }
+  const client = new MongoClient.connect(uri, function (err, db) {
+    //console.log("connext");
+    if (err) res.json("[]");
+    var dbo = db.db("mea");
+    // try{
+    var query = { _id: ObjectId(req.params.id) };
+    var newvalues = { $set: { encimage: req.body.image } };
+    dbo.collection("profile").updateOne(query, newvalues, function (err, result) {
+      if (err) res.json("[]");
+      //console.log(result);
+      res.json(result);
+      db.close();
+    });
+    // }catch(err){
+    // //console.log(err.stack);
+    // res.json("[]");}
+  });
+});
+
 
 
 
