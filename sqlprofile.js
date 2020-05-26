92d668341ad8edba595cfbc7bf5c0f469e053905
msc-dts <naphol159@gmail.com>
2020-05-26 19:15:26 +0700

    aa

--- sqlprofile.js
+++ sqlprofile.js
@@ -33,7 +33,7 @@ fs.createReadStream('profilepic.csv')
                 var request = new sql.Request();
 
                 // query to the database and get the records
-                request.input('@id', sql.VarChar, element.id).input('@title', sql.VarChar, element.title).input('@name', sql.VarChar, element.name).input('@surname', sql.VarChar, element.surname).input('@email', sql.VarChar, element.email).input('@position', sql.VarChar, element.position).query('insert into profile (id,title,name,surname,email,position) values (@id,@title,@name,@surname,@email,@position)', function (err, recordset) {
+                request.input('@id', sql.Int, element.id).input('@title', sql.VarChar, element.title).input('@name', sql.VarChar, element.name).input('@surname', sql.VarChar, element.surname).input('@email', sql.VarChar, element.email).input('@position', sql.VarChar, element.position).query('insert into profile (id,title,name,surname,email,position) values (@id,@title,@name,@surname,@email,@position)', function (err, recordset) {
 
                     if (err) console.log(err)
 
